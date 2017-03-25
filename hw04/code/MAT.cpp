287df462cf4968a3c8e92beaa0e6b263fae42fe7
Fu-En.Wang <fulton84717@gmail.com>
2017-03-25 18:50:30 +0800

    gg

---
+++ hw04/code/MAT.cpp
@@ -0,0 +1,445 @@
+// Matrix CPP
+// ID: 102061149
+// Name: Fu-En Wang
+
+#include "MAT.h"
+
+void Performance::Start(){
+    gettimeofday(&this->start, NULL);
+}
+
+double Performance::End(){
+    double duration;
+    gettimeofday(&this->stop, NULL);
+    duration = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000000.0;
+    return duration;
+}
+
+double Performance::End(const char right[], const char left[]){
+    double duration;
+    gettimeofday(&this->stop, NULL);
+    duration = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000000.0;
+    printf("%s %g %s\n", right, duration, left);
+    return duration;
+}
+
+Json::Json(const char f_name[], int flag){
+    this->first = 1;
+    this->ignore = flag;
+    if(!this->ignore){
+        this->f.open(f_name, ios::out);
+    }
+}
+
+void Json::Write(int data){
+    if(this->ignore)
+        return;
+    if(this->first == 1){
+        this->first = 0;
+        this->f << "[";
+    }
+    else
+        this->f << ",";
+    char s[100];
+    sprintf(s, "%d", data);
+    this->f << s;
+}
+
+void Json::Write(double data, int mode){
+    if(this->ignore)
+        return;
+    if(this->first == 1){
+        this->first = 0;
+        this->f << "[";
+    }
+    else
+        this->f << ",";
+    char s[100];
+    if(mode == 0)
+        sprintf(s, "%g", data);
+    else
+        sprintf(s, "%lf", data);
+    this->f << s;
+}
+
+void Json::Write(float data, int mode){
+    if(this->ignore)
+        return;
+    if(this->first == 1){
+        this->first = 0;
+        this->f << "[";
+    }
+    else
+        this->f << ",";
+    char s[100];
+    if(mode == 0)
+        sprintf(s, "%g", data);
+    else
+        sprintf(s, "%f", data);
+    this->f << s;
+}
+
+void Json::Write(const char data[]){
+    if(this->ignore)
+        return;
+    if(this->first == 1){
+        this->first = 0;
+        this->f << "[";
+    }
+    else
+        this->f << ",";
+    char s[100];
+    sprintf(s, "\"%s\"", data);
+    this->f << s;
+}
+
+void Json::Close(){
+    if(this->ignore)
+        return;
+    this->f << "]" << endl;
+    this->f.close();
+}
+
+MAT::MAT(int n){  // Square Matrix
+    this->m = n;
+    this->n = n;
+    this->val = new VEC*[m];
+    for(int i=0; i<n; i++)
+        this->val[i] = this->CreateVEC(n);
+}
+
+MAT::MAT(int m, int n){ // Rectangular Matrix
+    this->m = m;
+    this->n = n;
+    this->val = new VEC*[m];
+    for(int i=0; i<m; i++)
+        this->val[i] = this->CreateVEC(n);
+}
+
+MAT::MAT(const MAT &data){ // Copy matrix
+    this->m = data.m;
+    this->n = data.n;
+    this->val = new VEC*[m];
+    for(int i=0; i<m; i++){
+        this->val[i] = this->CreateVEC(n);
+        (*this)[i] = data[i];
+    } 
+}
+
+
+MAT::MAT(double **val, int m, int n){ // Matrix with initial value
+    this->m = m;
+    this->n = n;
+    this->val = new VEC*[m];
+    for(int i=0; i<m; i++){
+        this->val[i] = this->CreateVEC(n);
+        for(int j=0; j<n; j++)
+            (*this)[i][j] = val[i][j];
+    } 
+}
+
+void MAT::Reset(double **val, int m, int n){
+    this->m = m;
+    this->n = n;
+    this->val = new VEC*[m];
+    for(int i=0; i<m; i++){
+        this->val[i] = this->CreateVEC(n);
+        for(int j=0; j<n; j++)
+            (*this)[i][j] = val[i][j];
+    } 
+}
+
+MAT::~MAT(){
+    for(int i=0; i<this->n; i++){
+        this->val[i]->~VEC();
+        //delete this->val[i];
+    }
+    delete this->val;
+}
+
+void MAT::checkDim(const MAT &data){ // Check m,n match or not
+    if (this->m != data.m || this->n != data.n){
+        printf("MAT dimension dismatch!\n");
+        exit(0);
+    }
+    return;
+}
+
+VEC* MAT::CreateVEC(int n){
+    VEC *out = new VEC(n);
+    return out;
+}
+
+void MAT::operator=(const MAT &data){ // Assign maatrix value
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] = data[i][j];
+    return;
+}
+
+void MAT::operator=(double num){ // Assign maatrix value
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] = num;
+    return;
+}
+
+
+VEC& MAT::operator[](int index) const{ // Indexing: A[0], A[1]
+    if(index >= this->m || index < 0){
+        printf("MAT[index] out of bound!\n");
+        exit(0);
+    }
+    return *(this->val[index]);
+}
+
+void MAT::operator+=(const MAT &data){
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] += data[i][j];
+}
+void MAT::operator-=(const MAT &data){
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] -= data[i][j];
+
+}
+void MAT::operator*=(const MAT &data){
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] *= data[i][j];
+
+}
+void MAT::operator/=(const MAT &data){
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] /= data[i][j];
+
+}
+
+void MAT::operator+=(double num){
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] += num;
+}
+void MAT::operator-=(double num){
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] -= num;
+}
+void MAT::operator*=(double num){
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] *= num;
+}
+void MAT::operator/=(double num){
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            (*this)[i][j] /= num;
+}
+
+MAT MAT::operator+(const MAT &data){
+    MAT out(this->m, this->n);
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] + data[i][j];
+    return out;
+}
+MAT MAT::operator-(const MAT &data){
+    MAT out(this->m, this->n);
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] - data[i][j];
+    return out;
+}
+MAT MAT::operator*(const MAT &data){
+    if(this->n != data.m){
+        printf("MAT mul size dismatch!\n");
+        exit(0);
+    }
+    MAT out(this->m, data.n);
+    //this->checkDim(data);
+    /*
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] * data[i][j];
+    */
+    for(int i=0; i<this->m; i++){
+        for(int j=0; j<data.n; j++){
+            double sum = 0;
+            for(int k = 0; k < this->n; k++){
+                sum += (*this)[i][k] * data[k][j];
+            }
+            out[i][j] = sum;
+        }
+    }
+
+    return out;
+}
+
+VEC MAT::operator*(const VEC &data){
+    if(this->n != data.dim()){
+        printf("MAT mul size dismatch!\n");
+        exit(0);
+    }
+    VEC out(this->m);
+    //this->checkDim(data);
+    /*
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] * data[i][j];
+    */
+    for(int i=0; i<this->m; i++){
+        double sum = 0;
+        for(int k = 0; k < this->n; k++){
+            sum += (*this)[i][k] * data[k];
+        }
+        out[i] = sum;
+    }
+
+    return out;
+}
+
+
+MAT MAT::operator/(const MAT &data){
+    MAT out(this->m, this->n);
+    this->checkDim(data);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] / data[i][j];
+    return out;
+}
+
+MAT MAT::operator+(double num){
+    MAT out(this->m, this->n);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] + num;
+    return out;
+}
+MAT MAT::operator-(double num){
+    MAT out(this->m, this->n);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] - num;
+    return out;
+}
+MAT MAT::operator*(double num){
+    MAT out(this->m, this->n);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] * num;
+    return out;
+}
+MAT MAT::operator/(double num){
+    MAT out(this->m, this->n);
+    for(int i=0; i<this->m; i++)
+        for(int j=0; j<this->n; j++)
+            out[i][j] = (*this)[i][j] / num;
+    return out;
+}
+
+void MAT::print() const{
+    printf("----------------\n");
+    printf("%d %d\n", this->m, this->n);
+    for(int i=0; i<this->m; i++){
+        (*this)[i].print();
+        if(i != this->m-1)
+            printf("\n");
+    }
+    printf("----------------\n");
+}
+
+MAT MAT::T(){
+    MAT out(this->n, this->m);
+
+    for(int i=0; i<this->m; i++){
+        for(int j=0; j<this->n; j++){
+            out[i][j] = (*this)[j][i];
+        }
+    }
+    return out;
+}
+
+MAT operator+(double num, const MAT &data){
+    MAT out(data.m, data.n);
+    for(int i=0; i<data.m; i++)
+        for(int j=0; j<data.n; j++)
+            out[i][j] = num  + data[i][j];
+    return out;
+}
+MAT operator-(double num, const MAT &data){
+    MAT out(data.m, data.n);
+    for(int i=0; i<data.m; i++)
+        for(int j=0; j<data.n; j++)
+            out[i][j] = num  - data[i][j];
+    return out;
+}
+MAT operator*(double num, const MAT &data){
+    MAT out(data.m, data.n);
+    for(int i=0; i<data.m; i++)
+        for(int j=0; j<data.n; j++)
+            out[i][j] = num  * data[i][j];
+    return out;
+}
+MAT operator/(double num, const MAT &data){
+    MAT out(data.m, data.n);
+    for(int i=0; i<data.m; i++)
+        for(int j=0; j<data.n; j++)
+            out[i][j] = num  / data[i][j];
+    return out;
+}
+
+int MAT::GetM(){
+    return this->m;
+}
+int MAT::GetN(){
+    return this->n;
+}
+
+VEC fwdSubs(MAT &m1, VEC b){ // Forward Substitution
+    int m = m1.GetM();
+    VEC Y(b);
+    for(int i=0; i < m; i++){
+        for(int j=0; j < i; j++){
+            Y[i] -= m1[i][j] * Y[j];
+        }
+    }
+    return Y;
+}
+
+VEC bckSubs(MAT &m1, VEC b){ // Backward Substitution
+    int m=m1.GetM();
+    VEC X(b);
+    for(int i=m-1; i >= 0; i--){
+        for(int j=m-1; j > i; j--){
+            X[i] -= m1[i][j] * X[j];
+        }
+        X[i] /= m1[i][i];
+    }
+    return X;
+}
+
+MAT &luFact(MAT &m1){ // LU Decomposition
+    int m=m1.GetM();
+    for(int i=0; i<m; i++){
+        for(int j=i+1; j < m; j++){
+            m1[j][i] /= m1[i][i];
+        }
+        for(int j=i+1; j < m; j++){
+            for(int k=i+1; k < m; k++){
+                m1[j][k] -= m1[j][i] * m1[i][k];
+            }
+        }
+    }
+    return m1;
+}
+
+
---
+++ hw04/code/hw04.cpp
@@ -0,0 +1,130 @@
+// HW03: Resistor Networks
+// ID: 102061149
+// Name: Fu-En Wang
+
+#include "MAT.h"
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+
+using namespace std;
+
+double Solve(int node_one_side, double resistor);
+
+// Ax = b
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        printf("Please enter the resistor number one side\n");
+    }
+    int num_res = atoi(argv[1]);
+    int node_num_one_side = num_res + 1;
+    if(num_res == 2)
+        Solve(node_num_one_side, 1000);
+    else if(num_res == 4)
+        Solve(node_num_one_side, 500);
+    else if(num_res == 10)
+        Solve(node_num_one_side, 200);
+    else if(num_res == 20)
+        Solve(node_num_one_side, 100);
+    else if(num_res == 40)
+        Solve(node_num_one_side, 50);
+    else if(num_res == 50)
+        Solve(node_num_one_side, 40);
+    else
+        printf("Argument error!\n");
+    return 0;
+}
+
+double Solve(int node_one_side, double resistor){
+    struct timeval start, stop, a,b;
+    double conduct = 1 / resistor;
+    double MODEL, LU, FWD, BCK;
+    gettimeofday(&start, NULL);
+
+    MAT A(node_one_side*node_one_side, node_one_side*node_one_side);
+    VEC B(node_one_side*node_one_side);
+
+    A = 0;
+    // Process by row first
+    for(int i=0, node=0; i<node_one_side; i++){
+        for(int j=0; j<node_one_side-1; j++){
+            A[node][node] += conduct;
+            A[node][node+1] -= conduct;
+            A[node+1][node+1] += conduct;
+            A[node+1][node] -= conduct;
+            node++;
+        }
+        node++;
+    }
+    // Process column
+    for(int i=0, node=0; i<node_one_side-1; i++){
+        for(int j=0; j<node_one_side; j++){
+            A[node][node] += conduct;
+            A[node][node + node_one_side] -= conduct;
+            A[node + node_one_side][node + node_one_side] += conduct;
+            A[node + node_one_side][node] -= conduct;
+            node++;
+        }
+    }
+    int Vin_point = (node_one_side-1)/2;
+    int Ground_point = (node_one_side*node_one_side-1) - (node_one_side-1) / 2;
+    A[Vin_point] = 0;
+    A[Vin_point][Vin_point] = 1;
+    B[Vin_point] = 1;
+
+    A[Ground_point] = 0;
+    A[Ground_point][Ground_point] = 1;
+    gettimeofday(&stop, NULL);
+    MODEL = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000000.0;
+
+    //A.print();
+    gettimeofday(&a, NULL);
+    luFact(A);
+    gettimeofday(&b, NULL);
+    LU = (b.tv_sec - a.tv_sec) + (b.tv_usec - a.tv_usec) / 1000000.0;
+    
+    gettimeofday(&a, NULL);
+    VEC Y(fwdSubs(A, B));
+    gettimeofday(&b, NULL);
+    FWD = (b.tv_sec - a.tv_sec) + (b.tv_usec - a.tv_usec) / 1000000.0;
+
+    gettimeofday(&a, NULL);
+    VEC X(bckSubs(A, Y));
+    gettimeofday(&b, NULL);
+    BCK = (b.tv_sec - a.tv_sec) + (b.tv_usec - a.tv_usec) / 1000000.0;
+
+    //X.print();
+    double V = X[Vin_point];
+    double V_ne = X[node_one_side-1];
+    double V_ea = X[(node_one_side*node_one_side-1)/2];
+    double V_sw = X[node_one_side*node_one_side - node_one_side];
+    double I = 0;
+    double res;
+    I += (V - X[Vin_point-1]) * conduct;
+    I += (V - X[Vin_point+1]) * conduct;
+    I += (V - X[Vin_point+node_one_side]) * conduct;
+    
+    res = V / I; 
+    cout << "-----------------" << endl;
+    printf("Dimension: %d\n", node_one_side*node_one_side);
+    printf("Each resistance: %lf\n", resistor);
+    printf("Total resistance: %lf\n", res);
+    printf("V_ne: %lf\n", V_ne);
+    printf("V_ea: %lf\n", V_ea);
+    printf("V_sw: %lf\n", V_sw);
+    printf("MODEL(s): %g\n", MODEL);
+    printf("LU(s): %g\n", LU);
+    printf("FWD(s): %g\n", FWD);
+    printf("BCK(s): %g\n", BCK);
+    cout << "-----------------" << endl << endl;
+    return res;
+}
+
+
+
+
+
+
+
+
+
