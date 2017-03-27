5be243e97ae391ced3657efec751edb151505ef8
Fu-En.Wang <fulton84717@gmail.com>
2017-03-27 17:22:01 +0800

    gg

--- bin/Submit.py
+++ bin/Submit.py
@@ -7,7 +7,7 @@ f = open('config.txt', 'r')
 name = f.read().replace("\n", "")
 print name
 
-subprocess.call('mkdir submit', shell=True)
+subprocess.call('mkdir -p submit', shell=True)
 command = 'cp code/%s.cpp code/MAT.h code/MAT.cpp code/VEC.h code/VEC.cpp code/makefile doc/%sa.pdf submit/'%(name,name)
 
 subprocess.call(command, shell=True)
