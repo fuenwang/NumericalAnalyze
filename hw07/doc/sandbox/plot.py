f679a071ad8ecf546cfbb730b727b1966e317831
fuenwang <fulton84717@gmail.com>
2017-04-18 19:58:04 +0800

    gg

--- hw07/doc/sandbox/plot.py
+++ hw07/doc/sandbox/plot.py
@@ -2,38 +2,45 @@ import numpy as np
 from matplotlib import pyplot as plt
 import Parser
 
-data1 = Parser.ReadCSV('iter_num_unshifted.csv')
-data2 = Parser.ReadCSV('iter_avg_unshifted.csv')
-data3 = data1[:,1] * data2[:,1]
-#print data
-N1 = data1[:, 0]
-num1 = data3
+data = Parser.ReadCSV('eigen.csv')
+N = data[:, 0]
 
-data1 = Parser.ReadCSV('iter_num_shifted.csv')
-data2 = Parser.ReadCSV('iter_avg_shifted.csv')
-data3 = data1[:,1] * data2[:,1]
-
-N2 = data1[:,0]
-num2 = data3
-
-
+El1 = data[:, 1]
+El2 = data[:, 2]
+El3 = data[:, 3]
+Es1 = data[:, 4]
+Es2 = data[:, 5]
+Es3 = data[:, 6]
+'''
 plt.subplot('111')
-
-
-
-plt.plot(N1, num1, 'r--', marker='o', label='EVqr')
-
-plt.plot(N2, num2, 'b--', marker='o', label='EVqrShifted')
-
-plt.plot(N1, 10**(-6) * N1**5, 'm--', marker='o', label='$N^5$')
-plt.plot(N2, 10**(-8) * N2**4, 'y--', marker='o', label='$N^4$')
-
-plt.title('Overall complexity')
+plt.tight_layout()
+plt.plot(N, El1, 'r--', marker='o', label='E_large_1')
+plt.plot(N, El2, 'g--', marker='o', label='E_large_2')
+plt.plot(N, El3, 'b--', marker='o', label='E_large_3')
+plt.plot(N, 10*N**2, 'm--', marker='o', label='$N^2$')
+plt.title('Eigenvalue(largest)')
 plt.xlabel('N')
-plt.ylabel('Time(s)')
+plt.ylabel('Value')
 plt.xscale('log')
 plt.yscale('log')
 plt.legend()
-#plt.show()
+'''
 
-plt.savefig('overall.pdf', dpi=300, bbox_inches='tight')
+plt.subplot('111')
+plt.tight_layout()
+a = np.zeros(len(N)) + 0.5
+plt.plot(N, Es1, 'r--', marker='o', label='E_small_1')
+plt.plot(N, Es2, 'g--', marker='o', label='E_small_2')
+plt.plot(N, Es3, 'b--', marker='o', label='E_small_3')
+plt.plot(N, a, 'm--', label='y = 0.5')
+
+plt.title('Eigenvalue(smallest)')
+plt.xlabel('N')
+plt.ylabel('Value')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+
+
+#plt.show()
+plt.savefig('eigen_smallest.pdf', dpi=300, bbox_inches='tight')
