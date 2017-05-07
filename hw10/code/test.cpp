b0d51526e114324e0a87d9b564a484fcb8c09b36
fuenwang <fulton84717@gmail.com>
2017-05-08 07:09:25 +0800

    gg

--- hw10/code/test.cpp
+++ hw10/code/test.cpp
@@ -15,17 +15,17 @@ double func_der(double x){
 }
 
 int main(int argc, char *argv[]){
-    int order = 5;
+    int order = 3;
     VEC x(order);
     VEC poly(order + 1);
     VEC t(order + 1);
-    double a[] = {-10120200, -211502, 10118997, 211501, 1203, 1};
+    double a[] = {1,3,3,1};
     poly = a;
     poly.print();
     t.print();
     t = poly;
     t.print();
-    NewtonPoly(poly, x, 1000, 1e-9);
+    NewtonPoly(poly, x, 1000, 1e-10);
     x.print();
     return 0;
 }
