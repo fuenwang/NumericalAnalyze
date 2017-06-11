92e19922008344c9cae34370a8633fdd291d7a35
fuenwang <fulton84717@gmail.com>
2017-06-11 21:25:59 +0800

    spline_period

---
+++ mid3/hw09.cpp
@@ -0,0 +1,57 @@
+// HW09: Spline Interpolations
+// ID: 102061149
+// Name: Fu-En Wang
+
+#include "MAT.h"
+#include <vector>
+#include <fstream>
+#include <string>
+
+void Parse(const char f_name[], vector<double> &x, vector<double> &y){
+    ifstream f;
+    f.open(f_name);
+    string buf;
+    
+    getline(f, buf);
+    while(getline(f, buf)){
+        double a,b;
+        sscanf(buf.c_str(), "%lf %lf", &a, &b);
+        x.push_back(a);
+        y.push_back(b);
+    }
+    f.close();
+}
+
+int main(int argc, char *argv[]){
+    string f_name;
+    switch(argc){
+        case 1:
+            f_name = "f21.dat";
+            break;
+        case 2:
+            f_name = argv[1];
+            break;
+        default:
+            printf("Argument error!\n");
+            printf("Usage: ./a.out f21.dat\n");
+            exit(0);
+    }
+    vector<double> x;
+    vector<double> y;
+    Parse(f_name.c_str(), x, y);
+    VEC X((int)x.size());
+    VEC Y((int)y.size());
+    VEC M((int)y.size());
+    for(int i=0; i<X.dim(); i++){
+        X[i] = x[i];
+        Y[i] = y[i];
+    }
+    splineM_period(X.dim(), X, Y, M);
+    cout << spline(1, X.dim(), X, Y, M) << endl;
+    cout << spline(2, X.dim(), X, Y, M) << endl;
+    cout << spline(4, X.dim(), X, Y, M) << endl;
+    cout << spline(5, X.dim(), X, Y, M) << endl;
+    //M.print();
+
+    return 0;
+}
---
+++ mid3/hw12.cpp
@@ -0,0 +1,89 @@
+// HW12: RLC Circult
+// ID: 102061149
+// Name: Fu-En Wang
+
+#include "MAT.h"
+#include <vector>
+#include <fstream>
+#include <string>
+
+#define V 1
+#define R 1
+#define L 1
+#define C 1
+
+VEC Forward(VEC &x0, double h){
+    double V1_t = x0[0];
+    double V2_t = x0[1];
+    double iL_t = x0[2];
+    
+    x0[1] = (h / C) * iL_t + V2_t;
+    x0[2] = (h / L) * (V1_t - V2_t) + iL_t;
+    x0[0] = V - R * x0[2];
+
+    return x0;
+}
+
+VEC Backward(VEC &x0, double h){
+    double V2_t = x0[1];
+    double iL_t = x0[2];
+    
+    x0[2] = (iL_t - (h / L) * V2_t + (h * V) / L) / (1 + pow(h, 2) / (L * C) + (h * R) / L);
+    x0[0] = V - R * x0[2];
+    x0[1] = V2_t + (h / C) * x0[2];
+    return x0;
+}
+
+VEC Trap(VEC &x0, double h){
+    double V1_t = x0[0];
+    double V2_t = x0[1];
+    double iL_t = x0[2];
+    
+    double tmp0 = 1 + (h * h) / (4 * L * C) + (h * R) / (2 * L);
+    double tmp1 = 1 - (h * h) / (4 * L * C);
+    x0[2] = (tmp1 * iL_t + h / (2 * L) * (V + V1_t - 2 * V2_t)) / tmp0;
+    x0[0] = V - R * x0[2];
+    x0[1] = V2_t + (h / (2 * C)) * (x0[2] + iL_t);
+    return x0;
+}
+
+int main(int argc, char *argv[]){
+    VEC x0(3); // [v1, v2, iL]
+    /*    
+    x0[0] = 1; // v1(0) = 1
+    OrdDiff(Forward, x0, 0, 10, h); 
+    
+    
+    x0 = 0;
+    x0[0] = 1;
+    OrdDiff(Backward, x0, 0, 10, h);
+    
+    */
+    x0 = 0;
+    x0[0] = 1;
+    cout << "h = 0.1\n";
+    OrdDiff(Trap, x0, 0, 10, 0.1);
+    x0 = 0;
+    x0[0] = 1;
+    cout << "h = 0.01\n";
+    OrdDiff(Trap, x0, 0, 10, 0.01);
+    
+    //x0.print();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
